234c6b7bf1bae52ef4031d3a220f99082b477aba
Richter Brzeski <rmbrzeski@gmail.com>
2016-11-19 21:49:47 -0800

    Update ucla_plotter.js

--- Plotter/ucla_plotter.js
+++ Plotter/ucla_plotter.js
@@ -2,8 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var sleep = require('sleep-async')();
 
-var FILE_PATH = '/home/richter/ucla/Quadcopter-master/Plotter/flightpath.txt';
-var HTML_FILE_PATH = '/home/richter/ucla/Quadcopter-master/Plotter/index.html';
+There should be an error at line 5, it's just a reminder to set your file paths
+var FILE_PATH = '*******************************/flightpath.txt';
+var HTML_FILE_PATH = '***************/index.html';
 var lines_fp = 7;
 var file_data = "none";
 var f_sep = '';
